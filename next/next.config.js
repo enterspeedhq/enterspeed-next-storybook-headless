70fbb1481c6af5687472149367991c78442e5c94
Brian Jensen <bsj@enterspeed.com>
2022-04-26 14:55:58 +0200

    Updated variable name

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    ENTERSPEED_ENVIRONMENT_API_KEY: process.env.ENTERSPEED_ENVIRONMENT_API_KEY,
+    STORYBOOK_ENTERSPEED_ENVIRONMENT_API_KEY: process.env.STORYBOOK_ENTERSPEED_ENVIRONMENT_API_KEY,
   },
   reactStrictMode: true,
   trailingSlash: true
