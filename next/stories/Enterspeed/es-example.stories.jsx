37c8222e9c384a212d407dfee1cfa5965fc38f44
Brian Jensen <bsj@enterspeed.com>
2022-04-26 14:56:12 +0200

    Updated variable name

--- next/stories/Enterspeed/es-example.stories.jsx
+++ next/stories/Enterspeed/es-example.stories.jsx
@@ -3,6 +3,8 @@ import { withHeadless } from 'storybook-addon-headless'
 
 import EsExample from './es-example';
 
+console.log(process.env.STORYBOOK_ENTERSPEED_ENVIRONMENT_API_KEY);
+
 export default {
   title: 'Organisms/Enterspeed/Example',
   decorators: [
@@ -10,7 +12,7 @@ export default {
       restful: {
         baseURL: 'https://delivery.enterspeed.com/v1/?url=',
         headers: {
-          "X-Api-Key": process.env.ENTERSPEED_ENVIRONMENT_API_KEY
+          "X-Api-Key": process.env.STORYBOOK_ENTERSPEED_ENVIRONMENT_API_KEY
         }
       },
     }),
